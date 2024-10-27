ea8cd362ec95fa733d299fd5d7466e0326057027
Matsvei Alkhimovich <matalhim@gmail.com>
2024-10-27 19:28:52 +0300

    new file:   events/config.py 	new file:   events/db_connection.py 	new file:   events/graph_processing.py 	new file:   events/load_to_mongo.py 	new file:   events/processing.py     modified:   .gitignore

---
+++ events/config.py
@@ -0,0 +1,24 @@
+run_events_db = 'RUN_EVENTS'
+# ДБ НЕВОД-ШАЛ
+neas_db = 'NEAS_DB'
+# ДБ ДЕКОР
+decor_db = 'NEVOD_DB'
+# decor_db = 'RESULT1'
+# ДБ с резульатами
+result_db = 'RESULT1'
+# result_db = 'RESULT2'
+# временное окно
+delta_time = 1000
+# время для проверки
+check_time = 1000
+# число батчей
+BATCH = 1000
+# имя коллекции собранных ранов декор (вручную добавил все документы Gr3s40_... в одну коллекцию)
+runs_colllect = '_runs'
+statistica_collect = 'statistica'
+my_statistica_collect = 'my_statistica'
+
+# dates_list = ['2018-12-20']
+dates_list = ['2018-12-19', '2018-12-20']
+
+folder_path = r"dataset/run_events_812-841"
