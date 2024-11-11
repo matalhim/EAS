aafbb34b29dfc27c0811ce61c5593c97404e6505
Matsvei Alkhimovich <matalhim@gmail.com>
2024-11-12 02:53:53 +0300

    run_events from mongo to csv 	new file:   data/run_events.csv 	new file:   direction/main.ipynb 	new file:   direction/mongo_processing/create_csv.py 	new file:   direction/mongo_processing/db_connection.py

---
+++ direction/mongo_processing/db_connection.py
@@ -0,0 +1,30 @@
+import os
+from pymongo import MongoClient, errors
+
+
+class DatabaseConnection:
+    def __init__(self, mongo_url=None):
+        self.mongo_url = mongo_url or os.environ.get(
+            'MONGO_URL', 'mongodb://localhost:27017')
+        self.client = None
+        self.databases = {}
+        self.connect()
+
+    def connect(self):
+        try:
+            self.client = MongoClient(
+                self.mongo_url, serverSelectionTimeoutMS=5000)
+            self.client.admin.command('ping')
+        except errors.ConnectionFailure as e:
+            print(f"Ошибка подключения к MongoDB серверу: {e}")
+            self.client = None
+
+    def add_database(self, alias, db_name):
+        if self.client:
+            self.databases[alias] = self.client[db_name]
+        else:
+            print(f"Подключение к серверу MongoDB не установлено. Невозможно добавить базу данных: {
+                  db_name}")
+
+    def get_database(self, alias):
+        return self.databases.get(alias, None)
